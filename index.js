fa0f0d9e5444159346f842909f4e77f90504e927
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2025-02-14 21:04:51 +0600

    setup prisma

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import connectDB from './server/DB/databaseConfigs.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
 dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -35,20 +33,10 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Derive __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // Root route
 app.get('/', (req, res) => {
   res.send('Hello to online API');
 });
 
 // Start server and connect to the database
-app.listen(PORT, () => {
-  connectDB().catch((err) => {
-    console.error('Failed to connect to the database:', err);
-    process.exit(1); // Exit the process with a failure code if DB connection fails
-  });
-  console.log(`Server Running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
